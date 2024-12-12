6e39771775232834b9abb3a14d538fba5edd9775
Edem Kestan <ed.kestan@gmail.com>
2024-12-12 19:38:02 +0200

    fixed the burger-menu and video

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.querySelector('.menu-icon');
     const menu = document.querySelector('.menu');
+    const body = document.body;
 
     if (menuIcon && menu) {
         menuIcon.addEventListener('click', () => {
             menu.classList.toggle('_active'); // Активирует мобильное меню
             menuIcon.classList.toggle('_active'); // Анимация кнопки бургер-меню
+            body.classList.toggle('no-scroll'); // Блокирует прокрутку страницы
         });
     }
 });
